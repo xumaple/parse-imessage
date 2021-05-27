2b7e7f2452d05a336fba284fbb1af405d81d0c35
Maple Xu <maplexu2010@gmail.com>
2021-05-27 17:21:24 -0400

    Added optional contacts support

--- messages.py
+++ messages.py
@@ -4,6 +4,13 @@ import pwd
 import os
 from shutil import copyfile
 from datetime import datetime
+import argparse
+import re
+
+parser = argparse.ArgumentParser(description='Parse your messages')
+parser.add_argument('--use_contacts', '-c', action='store_true')
+useContacts = parser.parse_args().use_contacts
+    
 
 # make copy of database
 db_path = pwd.getpwuid(os.getuid()).pw_dir + '/Library/Messages/chat.db'
@@ -22,9 +29,51 @@ def call(s):
     cur.execute(s)
     options = set(cur.fetchall())
     d = {k[0]: k[1] for k in options}
-    for name in sorted(d.items(), key=lambda x: x[1])[-20:][::-1]:
-        print(name)
     return d
+
+def getPhoneNumber(s):
+    s = re.sub(r'\W+', '', s)
+    if len(s) == 10:
+        s = '1'+s
+    return s
+
+def print_call(d, contacts):
+    for name in sorted(d.items(), key=lambda x: x[1])[-20:][::-1]:
+        k, v = name
+        k = getPhoneNumber(k)
+        if useContacts:
+            k = contacts.get(k, k)
+        print('{}: {}'.format(k, v))
+
+# Source: https://github.com/ronaldoussoren/pyobjc/blob/master/pyobjc-framework-Contacts/Examples/print-contacts.py
+def getContacts(d):
+    if useContacts is None:
+        return {}
+    import Contacts
+
+    def print_info(contact, stop):
+        if contact.isKeyAvailable_(Contacts.CNContactGivenNameKey):
+            for num in contact.phoneNumbers():
+                name = contact.givenName()
+                if contact.isKeyAvailable_(Contacts.CNContactFamilyNameKey) and contact.familyName() != '':
+                    name += ' ' + contact.familyName()
+                d[getPhoneNumber(num.value().stringValue())] = name
+
+        else:
+            print("Contact without phone number")
+        return False
+    fetchRequest = Contacts.CNContactFetchRequest.alloc().initWithKeysToFetch_(
+        [Contacts.CNContactGivenNameKey, Contacts.CNContactFamilyNameKey, Contacts.CNContactPhoneNumbersKey]
+    )
+
+    store = Contacts.CNContactStore.alloc().init()
+    ok, error = store.enumerateContactsWithFetchRequest_error_usingBlock_(
+        fetchRequest, None, print_info
+    )
+    if not ok:
+        print("Fetching contacts failed", error)
+    return d
+
 
 d = call(
 """
@@ -41,3 +90,5 @@ ORDER BY
     message_count DESC;
 """
 )
+contacts = getContacts({})
+print_call(d, contacts)
